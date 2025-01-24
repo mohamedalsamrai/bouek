e9f1f92f8a8140ba154ac2fa069ebd7ba3e6daa0
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2025-01-24 20:37:27 +0300

    add Google Maps integration

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyAZ_1SaMGN2aeth9Ldt1O3P9rEHxgA3Zc0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
