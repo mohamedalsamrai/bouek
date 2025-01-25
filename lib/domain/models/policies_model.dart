3245ee215a2b3795dedd5f0e9b64c3afae607327
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2025-01-25 12:58:31 +0300

    complete domain layer

---
+++ lib/domain/models/policies_model.dart
@@ -0,0 +1,11 @@
+class Policies {
+  final String paymentType;
+  final String cancellationPolicy;
+  final bool refundable;
+
+  Policies(
+    this.paymentType,
+    this.cancellationPolicy,
+    this.refundable,
+  );
+}
---
+++ lib/domain/models/room_model.dart
@@ -0,0 +1,15 @@
+class Room {
+  final String type;
+  final String category;
+  final int beds;
+  final String bedType;
+  final String description;
+
+  Room(
+    this.type,
+    this.category,
+    this.beds,
+    this.bedType,
+    this.description,
+  );
+}
