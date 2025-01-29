eaa8ff7e70e9abf873e1d8735cd472a0d325fd64
mohamedalsamrai <st.compe.mohammed.a.f.awad@ced.nahrainuniv.edu.iq>
2025-01-29 20:34:08 +0300

    .

--- lib/app/utils/constants/colors.dart
+++ lib/app/utils/constants/colors.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 
 const Color primaryColor = Color(0xff065CCB);
 const Color textColor = Color(0xff090909);
-<<<<<<< HEAD
 const Color secondaryColor = Color(0xffAEAEB4);
 // const Color borderColor = Color(0xffE5E5E5);
-=======
 const Color textBlackLightColor = Color(0xff272727);
 // const Color secondaryColor = Color(0xffAEAEB4);
 // const Color borderColor = Color(0xffE5E5E5);
->>>>>>> origin/master
